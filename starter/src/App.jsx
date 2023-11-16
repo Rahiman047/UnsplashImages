ecbc8a939aa09e32676636afc188506d94064586
Rahiman <abdurrahiman100@gmail.com>
2023-11-16 12:02:12 +0530

    dark

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,4 +1,14 @@
+import Gallery from "./Gallery.jsx";
+import SearchForm from "./SearchForm.jsx";
+import ThemeToggle from "./ThemeToggle";
+
 const App = () => {
-  return <h1>Unsplash Images Starter</h1>;
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
 };
 export default App;
